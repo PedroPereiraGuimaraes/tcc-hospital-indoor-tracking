f2b61c3f4d3f667a87ab7b9bf2819ca1002bea61
PedroPereiraGuimaraes <pedro.pereira@gec.inatel.br>
2024-03-26 18:05:43 -0300

    build: added login page

--- front_end/lib/views/screens/LoginScreen.dart
+++ front_end/lib/views/screens/LoginScreen.dart
@@ -1,6 +1,10 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -10,68 +14,65 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Login'),
-        ),
-        body: Padding(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+        child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  border: OutlineInputBorder(),
-                ),
-              ),
+              _buildTextField('EMAIL', 'user@gmail.com', _emailController,
+                  Icons.email_outlined, false),
               const SizedBox(height: 20),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  border: OutlineInputBorder(),
-                ),
-              ),
+              _buildTextField('PASSWORD', '*****************',
+                  _passwordController, Icons.remove_red_eye_outlined, true),
               const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  String email = _emailController.text;
-                  String password = _passwordController.text;
-                  if (email.isNotEmpty && password.isNotEmpty) {
-                    print('Email: $email\nPassword: $password');
-                  } else {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text('Error'),
-                          content:
-                              Text('Please enter both email and password.'),
-                          actions: [
-                            ElevatedButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text('OK'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  }
-                },
-                child: const Text("Login"),
-              ),
             ],
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String title, String hint,
+      TextEditingController controller, IconData icon, bool isPassword) {
+    return TextField(
+      controller: controller,
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        labelText: title,
+        labelStyle: TextStyle(
+          color: Color.fromARGB(255, 0, 129, 223),
+          fontFamily: GoogleFonts.josefinSans().fontFamily,
+          fontSize: 15,
+        ),
+        hintText: hint,
+        hintStyle: TextStyle(
+          color: Colors.grey,
+          fontFamily: GoogleFonts.josefinSans().fontFamily,
+          fontSize: 18,
+        ),
+        suffixIcon: Icon(
+          icon,
+          color: Color.fromARGB(255, 0, 129, 223),
+        ),
+        filled: true,
+        fillColor: Colors.white,
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Color.fromARGB(255, 0, 129, 223),
+            width: 3.0,
+          ),
+        ),
+      ),
+    );
   }
 }

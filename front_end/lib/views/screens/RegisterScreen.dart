060bc22f573129c16c84efc1974d749e6f719b43
apsgois <apsgois@outlook.com>
2024-03-27 16:50:26 -0300

    Register

--- front_end/lib/views/screens/RegisterScreen.dart
+++ front_end/lib/views/screens/RegisterScreen.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -10,8 +12,190 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _registrationController = TextEditingController();
+
+  final Box _boxAccounts = Hive.box("accounts");
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+              child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Center(
+                      child: Container(
+                        width: 200,
+                        height: 150,
+                        //decoration: BoxDecoration(
+                        //borderRadius: BorderRadius.circular(40),
+                        //border: Border.all(color: Colors.blueGrey)),
+                        child: Image.asset('assets/logo.png'),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: TextFormField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                          hintText: 'Enter Name',
+                          labelText: 'Name',
+                          prefixIcon: Icon(
+                            Icons.person,
+                            color: Colors.blue,
+                          ),
+                          errorStyle: TextStyle(fontSize: 18.0),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(9.0)))),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _emailController,
+                      validator: MultiValidator([
+                        RequiredValidator(errorText: 'Enter email address'),
+                        EmailValidator(
+                            errorText: 'Please correct email filled'),
+                      ]),
+                      decoration: InputDecoration(
+                          hintText: 'Email',
+                          labelText: 'Email',
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: Colors.lightBlue,
+                          ),
+                          errorStyle: TextStyle(fontSize: 18.0),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(9.0)))),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _passwordController,
+                      validator: MultiValidator([
+                        RequiredValidator(errorText: 'Enter password'),
+                        MinLengthValidator(6,
+                            errorText:
+                                'password must be at least 6 digits long'),
+                      ]),
+                      decoration: InputDecoration(
+                          hintText: 'Password',
+                          labelText: 'Password',
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: Colors.blue,
+                          ),
+                          errorStyle: TextStyle(fontSize: 18.0),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(9.0)))),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _registrationController,
+                      validator: MultiValidator([
+                        RequiredValidator(
+                            errorText: 'Enter registration number'),
+                      ]),
+                      decoration: InputDecoration(
+                          hintText: 'Registration Number',
+                          labelText: 'Registration Number',
+                          prefixIcon: Icon(
+                            Icons.confirmation_number,
+                            color: Colors.blue,
+                          ),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(9)))),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(50),
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (_formKey.currentState?.validate() ?? false) {
+                            _boxAccounts.put(
+                              _emailController.text,
+                              _passwordController.text,
+                            );
+
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                width: 200,
+                                backgroundColor:
+                                    // blue color,
+                                    Colors.blue,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                behavior: SnackBarBehavior.floating,
+                                content: const Text("Registered Successfully"),
+                              ),
+                            );
+
+                            _formKey.currentState?.reset();
+
+                            Navigator.pop(context);
+                          }
+                        },
+                        // text color white for contrast
+                        child: const Text(
+                          "Register",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Already have an account?"),
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text(
+                              "Login",
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ))),
+    );
   }
 }

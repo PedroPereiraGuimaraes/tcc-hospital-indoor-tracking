fa3b63b20a10c8a94c0563559ebeaaeed96caaeb
FerNagata <fernanda.fn@gec.inatel.br>
2024-04-16 19:06:00 -0300

    adding new endpoint for user

--- back-end/src/api/routes/user.py
+++ back-end/src/api/routes/user.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from database.repository.user import UserDAO
-from api.models import User, User_login
+from api.models import User, User_login, New_user_admin
 
 router = APIRouter()
 
@@ -21,7 +21,27 @@ def create_new_user(new_user: User):
 @router.post("/login")
 def login(user_login:User_login):
     userDAO = UserDAO()
-    user = userDAO.login_authentication(user_login.register, user_login.password)
+    user_verify = userDAO.login_authentication(user_login.register, user_login.password)
 
-    return user
+    return user_verify
 
+@router.post("/change-user-admin")
+def changeAdmin(new_user_admin:New_user_admin):
+    userDAO = UserDAO()
+    user_verify = userDAO.change_admin(new_user_admin.register, new_user_admin.is_admin)
+
+    return user_verify
+
+@router.delete("/delete")
+def deleteUser(user_register: str):
+    userDAO = UserDAO()
+    status = userDAO.delete_user(user_register)
+
+    return status
+
+@router.put("/update")
+def update_user(update_user: User):
+    userDAO = UserDAO()
+    creation_status = userDAO.update_user(update_user)
+
+    return creation_status

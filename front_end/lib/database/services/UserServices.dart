ff543377161311c6c670bc2a5e60189373c2c6f6
PedroPGuimaraes <pedro.pereira@gec.inatel.br>
2024-05-07 22:11:34 -0300

    fix: added some api services

--- front_end/lib/database/services/UserServices.dart
+++ front_end/lib/database/services/UserServices.dart
@@ -1,17 +1,10 @@
-import 'dart:convert';
-import 'package:front_end/models/User.dart';
 import 'package:http/http.dart' as http;
-
-// fazer login na API
 
 Future<String> authenticate(String register, String password) async {
   print('register: $register');
-  final url = Uri.parse('http://172.26.0.2:8000/user/login');
-  final response = await http.post(
-    url,
-    body:
-        {"register": "pedro", "password": "123456"}
-  );
+  final url = Uri.parse('http://10.0.2.2:8000/user/login');
+  final response =
+      await http.post(url, body: {"register": "pedro", "password": "123456"});
   print('Response status: ${response}');
   if (response.statusCode == 200) {
     return 'Success';

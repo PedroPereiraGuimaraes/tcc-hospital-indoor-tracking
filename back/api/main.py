bf895f575a2d98dee10056bd6ae25f735db0a63f
Fernanda Nagata Ito <fernanda.fn@gec.inatel.br>
2024-05-25 17:46:04 -0300

    fixing route name

--- back/api/main.py
+++ back/api/main.py
@@ -14,7 +14,7 @@ api.add_middleware(
 )
 
 api.include_router(user.router, prefix="/user", tags=["user"])
-api.include_router(equipment.router, prefix="/equipament", tags=["equipment"])
+api.include_router(equipment.router, prefix="/equipment", tags=["equipment"])
 api.include_router(room.router, prefix="/room", tags=["room"])
 
 if __name__ == "__main__":

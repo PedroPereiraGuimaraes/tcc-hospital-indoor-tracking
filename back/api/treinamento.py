400028bb4e3022a54428bf747165ce7220fe9cf4
Mablemb <mablemb@gmail.com>
2024-06-06 16:44:57 -0300

    New models using new data
    Novos modelos treinados a partir dos dados novos

--- back/api/treinamento.py
+++ back/api/treinamento.py
@@ -1,19 +1,26 @@
 
 from autogluon.tabular import TabularDataset, TabularPredictor
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
-#Importar tabela de dados
-import pandas as pd
+# # Carregar o dataset
+# df = pd.read_csv(r'C:\Users\mable\Documents\tcc-hospital-indoor-tracking\back\trainingData\roomData\TabelaTratadaV2.csv')
 
-#Substitua 'caminho/do/seu/arquivo.csv' pelo caminho real do seu arquivo CSV
-caminho_do_arquivo = r'back\trainingData\roomData\TabelaTratada.csv'
-train_data = pd.read_csv(caminho_do_arquivo)
+# # Dividir os dados em conjuntos de treino e teste
+# train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+
+# # Salvar os conjuntos de treino e teste em arquivos CSV
+# train_df.to_csv('train_dataset.csv', index=False)
+# test_df.to_csv('test_dataset.csv', index=False)
+
+
+train_data = pd.read_csv(r'back\trainingData\roomData\train_dataset.csv')
 
 #Exibir as primeiras linhas do dataset
 print(train_data.head())
 
 #Escolher coluna de saída
-label = 'sala'
+label = 'Sala'
 
 #Observar coluna de saída
 print(train_data[label].describe())
@@ -22,7 +29,7 @@ print(train_data[label].describe())
 predictor = TabularPredictor(label=label).fit(train_data)
 
 #Testar modelos e obter avaliações
-test_data = TabularDataset(r'back\trainingData\roomData\TabelaTratada.csv')
+test_data = TabularDataset(r'back\trainingData\roomData\test_dataset.csv')
 
 y_pred = predictor.predict(test_data.drop(columns=[label]))
 y_pred.head()

de94730aa0075bb461dd53cd67db357d5cf86545
Fernanda Nagata Ito <fernanda.fn@gec.inatel.br>
2024-06-15 11:05:15 -0300

    adding new functions to equipment repository

--- back/api/src/database/repository/equipment.py
+++ back/api/src/database/repository/equipment.py
@@ -1,6 +1,7 @@
 from src.database.connection_db import Database
 import json
 from bson import json_util 
+from datetime import datetime
 
 class EquipmentDAO: # DAO - Data Access Object
     def __init__(self):
@@ -98,3 +99,36 @@ class EquipmentDAO: # DAO - Data Access Object
         except Exception as e:
             print(f"Houve um erro ao tentar atualizar o manutenção: {e}")
             return None
+
+    def get_current_room_and_date(self, esp_id):
+        try:
+            res = self.db.collection.find_one({"esp_id": esp_id},  {"current_room": 1, "current_date": 1})
+            # print("one equipment: ", res['current_room'])
+
+            return res
+        except Exception as e:
+            raise e
+        
+    def update_historic(self, esp_id, room, date):
+        try:
+            new_data = {
+                "room": room,
+                "inicial_date": date,
+            }
+            res = self.db.collection.update_one({"esp_id": esp_id}, {"$push": {"historic": new_data}})
+            print("one equipment: ", res)
+
+            return res
+        except Exception as e:
+            raise e
+    
+    def update_current_room(self, esp_id, room):
+        try:
+            date = datetime.now()
+            
+            res = self.db.collection.update_one({"esp_id": esp_id},{"$set": {"current_room": room, "current_date": date}})
+            print("one equipment type: ", type(res))
+
+            return res
+        except Exception as e:
+            raise e
